72079d8f957bdf047833396bf19389fe18dee6e2
E.Eremine <ereminaep127@gmail.com>
2022-01-25 11:29:00 +0300

    VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,32 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const Counter = {
+    data() {
+        return {
+            result: 0,
+            number1:0,
+            number2:0,
+            motion:''
+        }
+    },
+    methods: {
+        calc() {
+            switch(this.motion) {
+                case 'sum':
+                    this.result=this.number1 + this.number2
+                    break;
+                case 'subtract':
+                    this.result=this.number1-this.number2
+                    break;
+                case 'divide':
+                    this.result=this.number1/this.number2
+                    break; 
+                case 'multiply':
+                    this.result=this.number1*this.number2
+                    break;     
+                }
+            }
+        }
+    }
+
+createApp(Counter).mount('#app');
