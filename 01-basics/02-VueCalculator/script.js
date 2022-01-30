b295f290a1483f06c2ba8f46f4884132e9db6b97
ereminaep <57357282+ereminaep@users.noreply.github.com>
2022-01-30 19:32:22 +0300

    02-VueCalculator fix

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,39 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const Calc = {
+    data() {
+        return {
+            result: 0,
+            number1: 0,
+            number2: 0,
+            action: ''
+        }
+    },
+    watch: {
+        action: function(action) {
+            this.result = this.calc(this.number1, this.number2, action);
+        },
+        number1: function(number1) {
+            this.result = this.calc(number1, this.number2, this.action);
+        },
+        number2: function(number2) {
+            this.result = this.calc(this.number1, number2, this.action);
+        }
+    },
+    methods: {
+        calc: function(number1, number, action) {
+            switch (action) {
+                case 'sum':
+                    return this.number1 + this.number2
+                case 'subtract':
+                    return this.number1 - this.number2
+                case 'divide':
+                    return this.number1 / this.number2
+                case 'multiply':
+                    return this.number1 * this.number2
+            }
+        }
+    }
+}
+
+createApp(Calc).mount('#app')
