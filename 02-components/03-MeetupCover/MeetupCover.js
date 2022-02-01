ba82c41100afa998f27b491e08dc8ea2e3bf0605
ereminaep <57357282+ereminaep@users.noreply.github.com>
2022-02-01 13:09:47 +0300

    add MeetupCover component

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -1,10 +1,19 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
-  name: 'MeetupCover',
-
-  template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    name: 'MeetupCover',
+    props: {
+        title: {
+            type: String,
+            default: ''
+        },
+        image: {
+            type: String,
+            default: ''
+        }
+    },
+    template: `
+    <div class="meetup-cover" :style="{'--bg-url': image}">
+        <h1 class="meetup-cover__title">{{title}}</h1>
     </div>`,
 });
