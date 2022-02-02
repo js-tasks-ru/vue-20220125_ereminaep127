bb4124438dc7fe27be8ba1e3ecbad209d32c1509
ereminaep <57357282+ereminaep@users.noreply.github.com>
2022-02-02 19:46:48 +0300

    05-MeetupAgenda add

--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -1,23 +1,36 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+
+console.log(agendaItemIcons['registration']);
 
 export default defineComponent({
-  name: 'MeetupAgendaItem',
-
-  template: `
+    name: 'MeetupAgendaItem',
+    data() {
+        return {
+            agendaItemIcons: agendaItemIcons,
+            agendaItemDefaultTitles: agendaItemDefaultTitles
+        }
+    },
+    props: {
+        agendaItem: {
+            type: Object,
+            default: {}
+        }
+    },
+    template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img src="/assets/icons/icon-key.svg" class="icon" alt="key" />
+        <img :src="'../../src/assets/icons/icon-'+agendaItemIcons[this.agendaItem.type]+'.svg'" class="icon" alt="key" />
       </div>
-      <div class="agenda-item__col">00:00 - 00:00</div>
+      <div class="agenda-item__col">{{agendaItem.startsAt}} - {{agendaItem.endsAt}}</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">Title</h3>
-        <p class="agenda-item__talk">
-          <span>Talk Speaker</span>
+        <h3 class="agenda-item__title" v-text="agendaItem.title?agendaItem.title:agendaItemDefaultTitles[this.agendaItem.type]"></h3>
+        <p class="agenda-item__talk" v-if="agendaItem.type=='talk'">
+          <span>{{agendaItem.speaker}}</span>
           <span class="agenda-item__dot"></span>
-          <span class="agenda-item__lang">EN</span>
+          <span class="agenda-item__lang">{{agendaItem.language}}</span>
         </p>
-        <p>Description</p>
+        <p v-if="agendaItem.description">{{agendaItem.description}}</p>
       </div>
     </div>`,
 });
