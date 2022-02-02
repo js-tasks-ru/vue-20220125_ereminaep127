6d0f7e7b99ff1629c3de982635a384bd97cb0c43
ereminaep <57357282+ereminaep@users.noreply.github.com>
2022-02-02 23:22:25 +0300

    PageMeetup

--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -1,26 +1,70 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import MeetupView from '../06-MeetupView/MeetupView.js';
+
+import { fetchMeetupById } from './meetupService.js';
 
 export default defineComponent({
-  name: 'PageMeetup',
+    name: 'PageMeetup',
 
-  components: {
-    UiAlert,
-    UiContainer,
-  },
+    data() {
+        return {
+            meetup: {},
+            load: true,
+            error: false
+        }
+    },
 
-  template: `
+    components: {
+        UiAlert,
+        UiContainer,
+        MeetupView
+    },
+
+    props: {
+        meetupId: {
+            type: Number,
+            default: 1
+        }
+    },
+
+    watch: {
+        meetupId: function(meetupId) {
+            this.getMeetup(meetupId);
+        }
+    },
+
+    created() {
+        this.getMeetup(this.meetupId);
+    },
+
+    methods: {
+        getMeetup(meetupId) {
+            this.load = true;
+            fetchMeetupById(meetupId).then(
+                result => {
+                    this.load = this.error = false;
+                    this.meetup = result;
+                },
+                error => {
+                    this.load = false;
+                    this.error = error.message;
+                }
+            );
+        }
+    },
+
+    template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <meetup-view v-if="Object.keys(meetup).length&&!load&&!error" :meetup="meetup"></meetup-view>
 
-      <ui-container>
+      <ui-container v-if="load">
         <ui-alert>Загрузка...</ui-alert>
       </ui-container>
 
-      <ui-container>
-        <ui-alert>error</ui-alert>
+      <ui-container v-if="error">
+        <ui-alert>{{error}}</ui-alert>
       </ui-container>
     </div>`,
 });
