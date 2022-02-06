593398f07ae70996dd8980df448c1c6f613b9796
ereminaep <57357282+ereminaep@users.noreply.github.com>
2022-02-06 16:45:19 +0300

    03-VBindCover

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+    <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
 
 <script>
+const theme = {
+
+}
+
 export default {
   name: 'MeetupCover',
+  props:{
+    title: {
+      type: String,
+        default: ''
+      },
+    image: {
+      type: String,
+        default: ''
+      }
+  },
+  computed:{
+    backgroundImg(){
+      let image=this.image?'url("'+this.image+'")':'var(--default-cover)';
+      return 'linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), '+image;
+    }
+  }
 };
+
 </script>
 
 <style scoped>
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
+  background-image: v-bind(backgroundImg);
+
   /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
   display: flex;
   flex-direction: column;
