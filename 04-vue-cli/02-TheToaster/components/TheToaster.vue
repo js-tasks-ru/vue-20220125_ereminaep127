c58f6be4d6730139278118785ba1826cdca10aad
ereminaep <57357282+ereminaep@users.noreply.github.com>
2022-02-08 21:52:51 +0300

    lesson 04: 02-TheToaster

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,24 +1,53 @@
 <template>
   <div class="toasts">
-    <div class="toast toast_success">
-      <ui-icon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
-
-    <div class="toast toast_error">
-      <ui-icon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
-    </div>
+    <toast
+      v-for="toast in toasts"
+      :key="toast.id"
+      :type="toast.type"
+      :message="toast.message"
+      :icon="$options.settings.icons[toast.type]"
+    ></toast>
   </div>
 </template>
 
 <script>
-import UiIcon from './UiIcon';
+import Toast from './Toast';
 
 export default {
   name: 'TheToaster',
-
-  components: { UiIcon },
+  components: { Toast },
+  settings:{
+      icons:{
+      success:'check-circle',
+      error:'alert-circle'
+    }
+  },
+  data(){
+    return {
+      id:0,
+      toasts:[],
+    }
+  },
+  methods:{
+    addToast(message,type,time){
+      this.toasts.push({
+        id:this.id,
+        message:message,
+        type:type
+      });
+      setTimeout(() => this.hideToast(this.id), time);
+      this.id++;
+    },
+    hideToast(id){
+      this.toasts.pop();
+    },
+    error(message){
+      this.addToast(message,'error',5000);
+    },
+    success(message){
+      this.addToast(message,'success',5000);
+    }
+  }
 };
 </script>
 
@@ -39,35 +68,5 @@ export default {
     bottom: 72px;
     right: 112px;
   }
-}
-
-.toast {
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  width: auto;
-}
-
-.toast + .toast {
-  margin-top: 20px;
-}
-
-.toast__icon {
-  margin-right: 12px;
-}
-
-.toast.toast_success {
-  color: var(--green);
-}
-
-.toast.toast_error {
-  color: var(--red);
 }
 </style>
