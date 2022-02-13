20b095ad38bae4ab6e6149ebda2631208af5dced
ereminaep <57357282+ereminaep@users.noreply.github.com>
2022-02-13 11:56:29 +0300

    lesson5: 03-ScrollBehavior

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -1,44 +1,59 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 export const router = createRouter({
-  history: createWebHistory('/05-vue-router/03-ScrollBehavior'),
-
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: () => import('../views/PageMeetups'),
-    },
-    {
-      path: '/meetups',
-      name: 'meetups',
-      redirect: { name: 'index' },
-    },
-    {
-      path: '/meetups/:meetupId(\\d+)',
-      name: 'meetup',
-      meta: {
-        showReturnToMeetups: true,
-        saveScrollPosition: true,
-      },
-      props: true,
-      redirect: (to) => ({ name: 'meetup.description', params: to.params }),
-      component: () => import('../views/PageMeetup'),
-      children: [
-        {
-          path: '',
-          alias: 'description',
-          name: 'meetup.description',
-          props: true,
-          component: () => import('../views/PageMeetupDescription'),
+    history: createWebHistory('/05-vue-router/03-ScrollBehavior'),
+    routes: [{
+            path: '/',
+            name: 'index',
+            component: () =>
+                import ('../views/PageMeetups'),
         },
         {
-          path: 'agenda',
-          name: 'meetup.agenda',
-          props: true,
-          component: () => import('../views/PageMeetupAgenda'),
+            path: '/meetups',
+            name: 'meetups',
+            redirect: { name: 'index' },
         },
-      ],
-    },
-  ],
+        {
+            path: '/meetups/:meetupId(\\d+)',
+            name: 'meetup',
+            meta: {
+                showReturnToMeetups: true,
+                saveScrollPosition: true,
+            },
+            props: true,
+            redirect: (to) => ({ name: 'meetup.description', params: to.params }),
+            component: () =>
+                import ('../views/PageMeetup'),
+            children: [{
+                    path: '',
+                    alias: 'description',
+                    name: 'meetup.description',
+                    props: true,
+                    component: () =>
+                        import ('../views/PageMeetupDescription'),
+                },
+                {
+                    path: 'agenda',
+                    name: 'meetup.agenda',
+                    props: true,
+                    component: () =>
+                        import ('../views/PageMeetupAgenda'),
+                },
+            ],
+        },
+    ],
+    scrollBehavior(to, from, savedPosition) {
+        if (to.meta.saveScrollPosition && from.meta.saveScrollPosition) {
+            return false
+        }
+        if (to.hash) {
+            return {
+                el: to.hash,
+            }
+        } else if (savedPosition) {
+            return savedPosition
+        } else {
+            return { top: 0, left: 0 }
+        }
+    }
 });
